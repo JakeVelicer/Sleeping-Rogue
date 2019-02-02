42fca7a39e3f714f2c3c67d2c6b147b6c67b2439
Robyn Riley <rriley2@mail.bradley.edu>
2019-02-02 14:05:09 -0600

    Making Movement better
    Lots of stuff, added some things and changed a lot of things

--- Sleeping Rogue/Assets/PlatformerController.cs
+++ Sleeping Rogue/Assets/PlatformerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlatformerController : MonoBehaviour {
 
     [HideInInspector] public bool facingRight = true;
-    [HideInInspector] public bool jump = false;
+    public bool wallJumpEnabled = false;
     public float moveForce;
-    public float maxSpeed;
+    float maxSpeed;
+    public float groundSpeed;
+    public float airSpeed;
     public float jumpForce;
-    public Transform groundCheck;
+    public Transform groundCheck1, groundCheck2;
+    public Transform wallCheck1;
 
     public bool grounded = false;
     public bool wall = false;
@@ -19,63 +22,122 @@ public class PlatformerController : MonoBehaviour {
     public int jumps = 0;
     private int maxJumps = 1;
 
-    public float timer = 0.0f;
+    public float jumpTimer, wallJumpTimer = 0.0f;
     public static Vector3 check;
+
+    float horiz;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        horiz = 0;
     }
 
     // Update is called once per frame
     void Update () {
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
+        jumpTimer += Time.deltaTime;
+
+        grounded = Physics2D.Linecast(groundCheck2.position, groundCheck1.position, 1 << LayerMask.NameToLayer("Ground"));
+        wall = Physics2D.Linecast(transform.position, wallCheck1.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        
 
         if(Input.GetButtonDown("Jump") && jumps < maxJumps)
         {
-            jump = true;
+            jumpTimer = 0;
+            Jump();
             jumps++;
         }
 
         if (grounded)
         {
-            jumps = 0;
+            if (jumpTimer > .1)
+            {
+                jumps = 0;
+            }
         }
+        else if (wall && !grounded && rb2d.velocity.y <= 0)
+        {
+            wallJumpEnabled = true;
+            rb2d.velocity = Vector3.zero;
+            wallJumpTimer = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                WallJump();
+            }
+        }
+
+        if (!wall && !grounded)
+        {
+            maxSpeed = airSpeed;
+        }
+        else maxSpeed = groundSpeed;
+        
 	}
 
     private void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
+       
+            horiz = Input.GetAxis("Horizontal");
 
-        anim.SetFloat("Speed", Mathf.Abs(h));
-
-        if(h * rb2d.velocity.x < maxSpeed)
-        {
-            rb2d.AddForce(Vector2.right * h * moveForce);
-        }
-        if(Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-        {
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-        }
-
-        if(h > 0 && !facingRight)
+            anim.SetFloat("Speed", Mathf.Abs(horiz));
+       
+            if (horiz * rb2d.velocity.x < maxSpeed)
+            {
+                rb2d.AddForce(Vector2.right * horiz * moveForce);
+            }
+            if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            {
+                rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+            }
+        if(horiz > 0 && !facingRight)
         {
             Flip();
         }
-        else if (h < 0 && facingRight)
+        else if (horiz < 0 && facingRight)
         {
             Flip();
         }
+        
 
-        if (jump)
+        if (wallJumpEnabled)
         {
-            anim.SetTrigger("Jump");
-            rb2d.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
+            horiz = 0;
+            if (!wall)
+            {
+                wallJumpTimer += Time.deltaTime;
+            }
         }
+        
+        if(wallJumpTimer >= .25)
+        {
+            wallJumpEnabled = false;
+        }
+        
     }
+
+    void Jump()
+    {
+        anim.SetTrigger("Jump");
+        rb2d.AddForce(new Vector2(0f, jumpForce));
+    } 
+
+    void WallJump()
+    {
+        anim.SetTrigger("Jump");
+        if (facingRight)
+        {
+            rb2d.AddForce(new Vector2(-jumpForce * 1.3f, jumpForce * 1.2f));
+        } else
+        {
+
+            rb2d.AddForce(new Vector2(jumpForce * 1.3f, jumpForce * 1.2f));
+        }
+        Flip();
+    }
+    
 
     void Flip()
     {
@@ -83,5 +145,13 @@ public class PlatformerController : MonoBehaviour {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 11)
+        {
+            jumpTimer = 0;
+        }
     }
 }
