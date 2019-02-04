b256863b4dedd1795120227fed2115296543cbf9
Jake Velicer <007velicer@gmail.com>
2019-02-04 15:56:05 -0600

    Drones properly track nodes and players

--- Sleeping Rogue/Assets/Scripts/EnemyMovement.cs
+++ Sleeping Rogue/Assets/Scripts/EnemyMovement.cs
@@ -5,49 +5,51 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour {
 
     public GameObject current;
-
     public static GameObject player;
-
     public bool spotted;
-
-    public int speed;
+    public int roamSpeed;
+    public int trackSpeed;
+    private int speed;
 
 	// Use this for initialization
 	void Start () {
+
         spotted = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        speed = roamSpeed;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 look;
-
+        
+        //Vector2 look;
         float walkTime = speed * Time.deltaTime;
 
-        if (!spotted)
+        if (spotted && player.GetComponent<PlatformerController>().dreaming == false)
         {
-
-            look = new Vector2(current.transform.position.z, current.transform.position.y);
-            
+            //look = new Vector2(player.transform.position.z, player.transform.position.y);
+            speed = trackSpeed;
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, walkTime);
+            transform.right = -(player.transform.position - transform.position);
+        }
+        else if (!spotted || (spotted && player.GetComponent<PlatformerController>().dreaming == true))
+        {
+            //look = new Vector2(current.transform.position.z, current.transform.position.y);
+            speed = roamSpeed;
             transform.position = Vector2.MoveTowards(transform.position, current.transform.position, walkTime);
-
             transform.right = -(current.transform.position - transform.position);
             if (transform.position.Equals(current.transform.position))
             {
                 current = current.GetComponent<DirectionNode>().getNext();
             }
         }
-        else
-        {
-            look = new Vector2(player.transform.position.z, player.transform.position.y);
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, walkTime);
-            transform.right = -(player.transform.position - transform.position);
-        }
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Floor")
+        if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Floor")
         {
             current = current.GetComponent<DirectionNode>().getNext();
         }
--- Sleeping Rogue/Assets/Scripts/FOVScript.cs
+++ Sleeping Rogue/Assets/Scripts/FOVScript.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class FOVScript : MonoBehaviour {
 
+    private PlatformerController player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Shadow(Clone)")
+        if (collision.gameObject.name == "Player" && player.dreaming == false)
         {
-                GetComponentInParent<EnemyMovement>().spotted = true;
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    GetComponentInParent<EnemyMovement>().spotted = false;
-
-                }
+            GetComponentInParent<EnemyMovement>().spotted = true;
+            //if (Input.GetKeyDown(KeyCode.E))
+            //{
+            //    GetComponentInParent<EnemyMovement>().spotted = false;
+            //}
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Shadow(Clone)")
+        if (collision.gameObject.name == "Player")
         {
             GetComponentInParent<EnemyMovement>().spotted = false;
         }
@@ -27,14 +32,13 @@ public class FOVScript : MonoBehaviour {
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Shadow(Clone)")
+        if (collision.gameObject.name == "Player" && player.dreaming == false)
         {
-                GetComponentInParent<EnemyMovement>().spotted = true;
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    GetComponentInParent<EnemyMovement>().spotted = false;
-
-                }
+            GetComponentInParent<EnemyMovement>().spotted = true;
+            //if (Input.GetKeyDown(KeyCode.E))
+            //{
+            //    GetComponentInParent<EnemyMovement>().spotted = false;
+            //}
         }
     }
 }
