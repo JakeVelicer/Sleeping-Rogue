4e015d5417a458a6cddc5167339dd2c7a0bbe654
Jake Velicer <007velicer@gmail.com>
2019-02-04 23:52:02 -0600

    Dream Platform

--- Sleeping Rogue/Assets/Scripts/dreamObject.cs
+++ Sleeping Rogue/Assets/Scripts/dreamObject.cs
@@ -5,42 +5,31 @@ using UnityEngine;
 public class dreamObject : MonoBehaviour
 {
 
-    public SpriteRenderer spriteRenderer;
-    bool active;
-    Collider2D m_Collider;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D m_Collider;
+    private PlatformerController Player;
+
     // Use this for initialization
     void Start()
     {
-        this.spriteRenderer = GetComponent<SpriteRenderer>();
-        this.spriteRenderer.enabled = false;
-        active = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
         m_Collider = GetComponent<Collider2D>();
-        this.m_Collider.enabled = false;
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
         GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!PlayerMovement.dream)
-        {
-            this.spriteRenderer.enabled = false;
-            this.m_Collider.enabled = false;
-            active = false;
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-
-        }
-        else
+        if (Player.dreaming)
         {
             this.spriteRenderer.enabled = true;
             this.m_Collider.enabled = true;
-            active = true;
-            if (gameObject.tag == "Box")
-            {
-
-
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            }
+        }
+        else
+        {
+            this.spriteRenderer.enabled = false;
+            this.m_Collider.enabled = false;
         }
     }
 }
