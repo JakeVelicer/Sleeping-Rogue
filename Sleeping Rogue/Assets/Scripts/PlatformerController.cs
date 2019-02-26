ea9223fffc7f1c0291990ee7c547c76b29b61290
Robyn Riley <rriley2@mail.bradley.edu>
2019-02-25 23:42:21 -0600

    Fuck yea motherfuckers we've got some GOD DAMN MENUS. FUCKING KILL ME.
    W H Y WAS THIS THE HARD PART

--- Sleeping Rogue/Assets/Scripts/PlatformerController.cs
+++ Sleeping Rogue/Assets/Scripts/PlatformerController.cs
@@ -406,16 +406,6 @@ public class PlatformerController : MonoBehaviour {
             menuOptions.SetActive(false);
             velocHolder = rb2d.velocity;
             rb2d.bodyType = RigidbodyType2D.Static;
-
-            MenuScript.Pause = FindObjectsOfType<Button>();
-            Button temp = MenuScript.Pause[0];
-            MenuScript.Pause[0] = MenuScript.Pause[1];
-            MenuScript.Pause[1] = temp;
-            for(int i = 0; i < MenuScript.Pause.Length; i++)
-            {
-                Debug.Log(MenuScript.Pause[i]);
-            }
-            MenuScript.Pause[0].Select();
         }
         else
         {
