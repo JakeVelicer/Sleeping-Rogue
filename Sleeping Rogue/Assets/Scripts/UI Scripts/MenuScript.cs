e8ade08ef773d3d3f9f9ddf5f98eb5c3a310912f
Blake Messmer <scarclelyucky@gmail.com>
2019-02-25 18:03:43 -0600

    Main Menu and End Level Prefab

---
+++ Sleeping Rogue/Assets/Scripts/UI Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void Resume()
+    {
+        PlatformerController.paused = false;
+    }
+}
