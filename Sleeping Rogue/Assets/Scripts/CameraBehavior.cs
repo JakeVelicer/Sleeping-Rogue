c6c5282374274daffe6d1077daf0543bf08a74b4
Jake Velicer <007velicer@gmail.com>
2019-02-28 14:43:07 -0600

    Wall added, Camera automatically finds player

---
+++ Sleeping Rogue/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    private Transform Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        gameObject.GetComponent<CinemachineVirtualCamera>().Follow = Player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
