614d2ac82bce23e56ad81bea260d133ea880a51a
Jake Velicer <007velicer@gmail.com>
2019-02-28 20:49:53 -0600

    Camera Zoom Out
    Use 6 on keyboard, moving around camera in zoom out not implemented yet

--- Sleeping Rogue/Assets/Scripts/CameraBehavior.cs
+++ Sleeping Rogue/Assets/Scripts/CameraBehavior.cs
@@ -6,17 +6,52 @@ using Cinemachine;
 public class CameraBehavior : MonoBehaviour
 {
     private Transform Player;
+    private CinemachineVirtualCamera CameraController;
+    public float NormalZoom;
+    public float ZoomOut;
+    private float ZoomAmount;
+    private bool Switching;
 
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
-        gameObject.GetComponent<CinemachineVirtualCamera>().Follow = Player;
+        CameraController = gameObject.GetComponent<CinemachineVirtualCamera>();
+        CameraController.Follow = Player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("CameraZoom")) {
+            if (!Switching) {
+                StartCoroutine(ZoomController());
+            }
+        }
+
+    }
+
+    private IEnumerator ZoomController() {
+
+        var CurrentSize = CameraController.m_Lens.OrthographicSize;
+        Switching = true;
+
+        if (CurrentSize >= ZoomOut) {
+            ZoomAmount = NormalZoom;
+            Player.GetComponent<PlatformerController>().canMove = true;
+            while (CameraController.m_Lens.OrthographicSize > ZoomAmount) {
+                CameraController.m_Lens.OrthographicSize -= 1;
+                yield return new WaitForSeconds(0.001f);
+            }
+        }
+        else if (CurrentSize <= NormalZoom) {
+            ZoomAmount = ZoomOut;
+            Player.GetComponent<PlatformerController>().canMove = false;
+            while (CameraController.m_Lens.OrthographicSize < ZoomAmount) {
+                CameraController.m_Lens.OrthographicSize += 1;
+                yield return new WaitForSeconds(0.001f);
+            }
+        }
+        Switching = false;
     }
 }
