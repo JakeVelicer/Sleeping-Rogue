66216e47fac3cafb598c0f2f190174199a3e4f5b
Jake Velicer <007velicer@gmail.com>
2019-03-05 14:35:51 -0600

    Text boxes, work with button now.

--- Sleeping Rogue/Assets/Scripts/ActivateDeactivateObject.cs
+++ Sleeping Rogue/Assets/Scripts/ActivateDeactivateObject.cs
@@ -5,27 +5,61 @@ using UnityEngine;
 public class ActivateDeactivateObject :InteractableObject
 {
 
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer1;
+    private SpriteRenderer spriteRenderer2;
     private Collider2D m_Collider;
+    private MeshRenderer m_meshRenderer;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        m_Collider = GetComponent<Collider2D>();
+        if (GetComponent<SpriteRenderer>() != null) {
+            spriteRenderer1 = GetComponent<SpriteRenderer>();
+        }
+        if (GetComponent<Collider2D>() != null) {
+            m_Collider = GetComponent<Collider2D>();
+        }
+        if (gameObject.transform.GetChild(0).GetComponent<MeshRenderer>() != null) {
+            m_meshRenderer = gameObject.transform.GetChild(0).GetComponent<MeshRenderer>();
+        }
+        if (gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>() != null) {
+           spriteRenderer2 = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        }
     }
 
 	// Update is called once per frame
 	void Update () {
+
+        Debug.Log(m_meshRenderer);
         
         if (isActive)
         {
-            this.spriteRenderer.enabled = true;
-            this.m_Collider.enabled = true;
+            if (spriteRenderer1 != null) {
+                this.spriteRenderer1.enabled = true;
+            }
+            if (spriteRenderer2 != null) {
+                this.spriteRenderer2.enabled = true;
+            }
+            if (m_Collider != null) {
+                this.m_Collider.enabled = true;
+            }
+            if (m_meshRenderer != null) {
+                this.m_meshRenderer.enabled = true;
+            }
         }
         else
         {
-            this.spriteRenderer.enabled = false;
-            this.m_Collider.enabled = false;
+            if (spriteRenderer1 != null) {
+                this.spriteRenderer1.enabled = false;
+            }
+            if (spriteRenderer2 != null) {
+                this.spriteRenderer2.enabled = false;
+            }
+            if (m_Collider != null) {
+                this.m_Collider.enabled = false;
+            }
+            if (m_meshRenderer != null) {
+                this.m_meshRenderer.enabled = false;
+            }
         }
 	}
 }
