613854354799781d9d06bb52c5dcfeb5dd8d1d3c
Jake Velicer <007velicer@gmail.com>
2019-03-05 14:46:23 -0600

    Locking on platform updated, spikes on platforms works

--- Sleeping Rogue/Assets/Scripts/ActivateDeactivateObject.cs
+++ Sleeping Rogue/Assets/Scripts/ActivateDeactivateObject.cs
@@ -7,7 +7,8 @@ public class ActivateDeactivateObject :InteractableObject
 
     private SpriteRenderer spriteRenderer1;
     private SpriteRenderer spriteRenderer2;
-    private Collider2D m_Collider;
+    private Collider2D m_Collider1;
+    private Collider2D m_Collider2;
     private MeshRenderer m_meshRenderer;
 
     void Start()
@@ -16,13 +17,16 @@ public class ActivateDeactivateObject :InteractableObject
             spriteRenderer1 = GetComponent<SpriteRenderer>();
         }
         if (GetComponent<Collider2D>() != null) {
-            m_Collider = GetComponent<Collider2D>();
+            m_Collider1 = GetComponent<Collider2D>();
         }
         if (gameObject.transform.GetChild(0).GetComponent<MeshRenderer>() != null) {
             m_meshRenderer = gameObject.transform.GetChild(0).GetComponent<MeshRenderer>();
         }
         if (gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>() != null) {
            spriteRenderer2 = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        }
+        if (gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>() != null) {
+           m_Collider2 = gameObject.transform.GetChild(0).GetComponent<Collider2D>();
         }
     }
 
@@ -37,8 +41,11 @@ public class ActivateDeactivateObject :InteractableObject
             if (spriteRenderer2 != null) {
                 this.spriteRenderer2.enabled = true;
             }
-            if (m_Collider != null) {
-                this.m_Collider.enabled = true;
+            if (m_Collider1 != null) {
+                this.m_Collider1.enabled = true;
+            }
+            if (m_Collider2 != null) {
+                this.m_Collider2.enabled = true;
             }
             if (m_meshRenderer != null) {
                 this.m_meshRenderer.enabled = true;
@@ -52,8 +59,11 @@ public class ActivateDeactivateObject :InteractableObject
             if (spriteRenderer2 != null) {
                 this.spriteRenderer2.enabled = false;
             }
-            if (m_Collider != null) {
-                this.m_Collider.enabled = false;
+            if (m_Collider1 != null) {
+                this.m_Collider1.enabled = false;
+            }
+            if (m_Collider2 != null) {
+                this.m_Collider2.enabled = false;
             }
             if (m_meshRenderer != null) {
                 this.m_meshRenderer.enabled = false;
