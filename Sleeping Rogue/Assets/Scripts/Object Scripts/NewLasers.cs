a3f7b89ddb68ec57a1c80d81bfc6094069855328
Robyn Riley <rriley2@mail.bradley.edu>
2019-03-12 20:55:24 -0500

    Lasers Update. Art in, extends and collides correctly

---
+++ Sleeping Rogue/Assets/Scripts/Object Scripts/NewLasers.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLasers : InteractableObject
+{
+
+    private Transform line;
+    public Transform hit;
+
+    public float width = .1f;
+    public float maxLength = 10.0f;
+    public float length;
+
+    private PlatformerController playerScript;
+
+    LayerMask collides;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = this.transform;
+        //line.useWorldSpace = false;
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlatformerController>();
+        length = 0;
+        line.localScale = new Vector3( width, line.localScale.y, line.localScale.z);
+    }
+
+    RaycastHit2D ray;
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Physics2D.Raycast(transform.position, transform.up, length, collides);
+        Debug.DrawRay(transform.position, transform.up, Color.blue);
+       if(ray.collider != null)
+        {
+            hit.position = ray.point;
+            length = hit.localPosition.y;
+        }
+       else if(length < maxLength)
+        {
+            StartCoroutine(drawLaser());
+        }
+
+        if (!isActive)
+        {
+            line.GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+            length = 0.1f;
+        }
+        else
+        {
+            line.GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<BoxCollider2D>().enabled = true;
+
+            StartCoroutine(drawLaser());
+        }
+
+        if (playerScript.dreaming == true)
+        {
+            this.gameObject.layer = 9;
+
+            collides = ~LayerMask.GetMask("Laser", "Background", "UI", "Sleep", "Player");
+        }
+        else
+        {
+            this.gameObject.layer = 8;
+
+            collides = ~LayerMask.GetMask("Laser", "Background", "UI", "Sleep");
+        }
+        line.GetComponent<SpriteRenderer>().size = new Vector2(line.GetComponent<SpriteRenderer>().size.x, length);
+        
+
+        GetComponent<BoxCollider2D>().size = new Vector2(width, length);
+        GetComponent<BoxCollider2D>().offset = new Vector2(0, length/2);
+    }
+
+    IEnumerator drawLaser()
+    {
+        while(length < maxLength && ray.collider == null)
+        {
+            length += .25f * Time.deltaTime;
+            hit.localPosition = new Vector3(0, length, 0);
+            yield return null;
+        }
+    }
+}
