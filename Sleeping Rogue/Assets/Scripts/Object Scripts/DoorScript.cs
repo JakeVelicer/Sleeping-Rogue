f1f16c6729cf521e4468011d7eb57d0f0a573f51
Jake Velicer <007velicer@gmail.com>
2019-04-11 23:29:07 -0500

    All doors don’t need to be set any more

--- Sleeping Rogue/Assets/Scripts/Object Scripts/DoorScript.cs
+++ Sleeping Rogue/Assets/Scripts/Object Scripts/DoorScript.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class DoorScript : InteractableObject {
 
     public bool MoveUp;
-    public float MoveDistance;
     private  bool CanMove;
 
+    private SpriteRenderer render;
     public AudioClip door;
     public AudioSource audioSource;
 
+    void Start() {
+
+        render = gameObject.GetComponent<SpriteRenderer>();
+    }
+
     // Update is called once per frame
     void Update () {
+        
         if (isActive && !CanMove)
         {
             StartCoroutine(Activation());
@@ -22,7 +28,7 @@ public class DoorScript : InteractableObject {
     private IEnumerator Activation() {
         isActive = false;
         CanMove = true;
-        for (int i = 0; i < MoveDistance; i++) {
+        for (int i = 0; i < (render.bounds.size.y * 2); i++) {
             audioSource.PlayOneShot(door);
             if (MoveUp) {
                 transform.Translate(Vector3.up * 0.5f, Space.World);
@@ -30,7 +36,7 @@ public class DoorScript : InteractableObject {
             else if (!MoveUp) {
                 transform.Translate(Vector3.down * 0.5f, Space.World);
             }
-            yield return new WaitForSeconds(0.001f);
+            yield return new WaitForSeconds(0.0005f);
         }
         if (MoveUp) {
             MoveUp = false;
