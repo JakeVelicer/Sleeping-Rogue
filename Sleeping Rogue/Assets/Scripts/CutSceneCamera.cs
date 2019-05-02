a59e7f073b909b9492038451ea698bb9e3cfc51d
Jake Velicer <007velicer@gmail.com>
2019-05-02 12:10:36 -0500

    CRAWL for levels

--- Sleeping Rogue/Assets/Scripts/CutSceneCamera.cs
+++ Sleeping Rogue/Assets/Scripts/CutSceneCamera.cs
@@ -20,7 +20,9 @@ public class CutSceneCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        if (Input.GetButtonDown("Submit")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     private IEnumerator CameraMovement() {
