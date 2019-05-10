dbb01e0fb661115aa146353f09009e0978090a96
Ryan Voss <rvoss@mail.bradley.edu>
2019-05-09 22:39:35 -0500

    Revert "LEvel X and return to body audio"
    This reverts commit f1b0eb67eacb0447b07cd5e39a0bcdd609bfd8ac.

--- Sleeping Rogue/Assets/Scripts/PlatformerController.cs
+++ Sleeping Rogue/Assets/Scripts/PlatformerController.cs
@@ -38,12 +38,9 @@ public class PlatformerController : MonoBehaviour {
     public AudioClip landing;
     public AudioClip sizzle;
     public AudioClip collect;
-    public AudioClip twinkle;
-    public AudioClip dreamEnd;
 
     public AudioSource audioSource;
     public AudioSource externals;
-    public AudioSource returnToDream;
 
     [Header("Performance Checks")]
     public bool step = true;
@@ -518,15 +515,12 @@ public class PlatformerController : MonoBehaviour {
                 }
                 returnEffect.transform.LookAt(ShadowInScene.transform.position);
                 returnEffect.Play();
-               
                 float Distance = 
                 Mathf.Abs(Vector3.Distance(ShadowInScene.transform.position, transform.position));
                 if (Distance <= 0.1f)
                 {
                     rb2d.velocity = Vector3.zero;
                     returnEffect.Stop();
-                    returnToDream.Stop();
-                    returnToDream.PlayOneShot(dreamEnd,.7f);
                     returnEffect.Clear();
                     movingToBody = false;
                     Destroy(ShadowInScene);
@@ -569,8 +563,6 @@ public class PlatformerController : MonoBehaviour {
 
             canMove = false;
             movingToBody = true;
-            returnToDream.Play();
-
             playerCollider.enabled = false;
             canDream = true;
             //CancelInvoke("Wave");
@@ -846,16 +838,7 @@ public class PlatformerController : MonoBehaviour {
         yield return new WaitForSeconds(.2f);
         step = true;
     }
-    private IEnumerator flyingSound()
-    {
-        audioSource.PlayOneShot(twinkle);
 
-        while (movingToBody == true)
-        {
-            yield return new WaitForSeconds(2f);
-            audioSource.PlayOneShot(twinkle);
-        }
-    }
 
     Save currSave;
 
