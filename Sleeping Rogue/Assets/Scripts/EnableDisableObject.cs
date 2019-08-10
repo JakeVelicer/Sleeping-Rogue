5bb10acaf9823dde9289745f53440a83edbf0a6a
Jake Velicer <007velicer@gmail.com>
2019-08-10 17:09:39 -0500

    Fixed framerate issues with Level X

---
+++ Sleeping Rogue/Assets/Scripts/EnableDisableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableObject : MonoBehaviour
+{
+    public GameObject[] connected;
+    private bool Triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.gameObject.tag == "Player") {
+            if (!Triggered) {
+                Triggered = true;
+                foreach(GameObject i in connected)
+                {
+                    if (i.activeSelf == true)
+                    {
+                        i.SetActive(false);
+                    }
+                    else
+                    {
+                        i.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
